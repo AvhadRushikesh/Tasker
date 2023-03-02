1d4a6ebf07b8a507761e3daeeef108c12fa93379
rushi-smb <rushi@smbios.co.in>
2023-03-02 16:16:13 +0530

    Add New Task

--- Tasker/MVVM/Views/NewTaskView.xaml.cs
+++ Tasker/MVVM/Views/NewTaskView.xaml.cs
@@ -1,3 +1,6 @@
+using Tasker.MVVM.Models;
+using Tasker.MVVM.ViewModels;
+
 namespace Tasker.MVVM.Views;
 
 public partial class NewTaskView : ContentPage
@@ -6,4 +9,28 @@ public partial class NewTaskView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AddTaskClicked(object sender, EventArgs e)
+    {
+		var vm = BindingContext as NewTaskViewModel;
+
+		var selectedCategory = 
+			vm.Categories.Where(x => x.IsSelected == true).FirstOrDefault();
+
+		if (selectedCategory != null)
+		{
+			var task = new MyTask
+			{
+				TaskName = vm.Task,
+				CatogoryId = selectedCategory.Id
+			};
+			vm.Tasks.Add(task);
+			await Navigation.PopAsync();
+		}
+		else
+		{
+			// If No category selected for task adding
+			await DisplayAlert("Invalid Selection", "You must select a category", "Ok");
+		}
+    }
 }
