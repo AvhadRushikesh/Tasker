79e8a6af9b21691f2e747c8b7276da6cc5651862
rushi-smb <rushi@smbios.co.in>
2023-03-02 14:12:24 +0530

    Create Category Section - See done and pending task and Add color to progress bar

--- Tasker/MVVM/ViewModels/MainViewModel.cs
+++ Tasker/MVVM/ViewModels/MainViewModel.cs
@@ -12,6 +12,8 @@ namespace Tasker.MVVM.ViewModels
     {
         public ObservableCollection<Category> Categories { get; set; }
 
+        public ObservableCollection<MyTask> Tasks { get; set; }
+
         public MainViewModel()
         {
             FillData();
@@ -40,6 +42,83 @@ namespace Tasker.MVVM.ViewModels
                     Color = "#14df80"
                 }
             };
+
+            Tasks = new ObservableCollection<MyTask>
+            {
+                new MyTask
+                {
+                    TaskName = "Upload exercise files",
+                    Completed = false,
+                    CatogoryId = 1
+                },
+                new MyTask
+                {
+                    TaskName = "Plan next course",
+                    Completed = false,
+                    CatogoryId = 1
+                },
+                new MyTask
+                {
+                    TaskName = "Upload new ASP.NET video on YouTube",
+                    Completed = false,
+                    CatogoryId = 2
+                },
+                new MyTask
+                {
+                    TaskName = "Fix Setting.cs class of the Project",
+                    Completed = false,
+                    CatogoryId = 2
+                },
+                new MyTask
+                {
+                    TaskName = "Update github repository",
+                    Completed = true,
+                    CatogoryId = 2
+                },
+                new MyTask
+                {
+                    TaskName = "Buy Product",
+                    Completed = false,
+                    CatogoryId = 3
+                },
+                new MyTask
+                {
+                    TaskName = "Go for the pepperoni pizza",
+                    Completed = false,
+                    CatogoryId = 3
+                }
+            };
+
+            UpdateData();   //  Get Pending Tasks / Percentage and TaskColor
+        }
+
+        public void UpdateData()
+        {
+            foreach (var c in Categories)
+            {
+                var tasks = from t in Tasks
+                            where t.CatogoryId == c.Id
+                            select t;
+
+                var completed = from t in tasks
+                                where t.Completed == true
+                                select t;
+
+                var notCompleted = from t in tasks
+                                   where t.Completed == false
+                                   select t;
+
+                c.PendingTasks = notCompleted.Count();
+                c.Percentage = (float)completed.Count() / (float)tasks.Count();
+            }
+            foreach (var t in Tasks)
+            {
+                var catColor =
+                    (from c in Categories
+                    where c.Id == t.CatogoryId
+                    select c.Color).FirstOrDefault();
+                t.TaskColor = catColor;
+            }
         }
     }
 }
